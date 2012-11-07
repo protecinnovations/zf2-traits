c1e155b9ba125bce2d2cffe97855959117b8e638
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2012-11-07 13:27:20 +0000

    Update travis dependencies

--- ci/dependencies.sh
+++ ci/dependencies.sh
@@ -3,3 +3,7 @@
 # Composer
 curl -s http://getcomposer.org/installer | php
 COMPOSER_PROCESS_TIMEOUT=4000 php -dapc.enable_cli=0 composer.phar -v install
+
+# phpcs
+pyrus install pear/PHP_CodeSniffer
+phpenv rehash
