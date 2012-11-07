76226ed47d6e8adb3821c485f2f89c37679834e7
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2012-11-07 11:33:19 +0000

    Add Blank Module to add this to ZF2 Modules site

---
+++ Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Protec\ZF2Trait;
+
+/**
+ * Module
+ *
+ * @package   ZF2Trait
+ * @author    Protec Innovations <support@protecinnovations.co.uk>
+ * @copyright 2012 Protec Innovations
+ */
+class Module
+{}
